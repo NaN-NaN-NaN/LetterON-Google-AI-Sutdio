cce48d9cbd72286c9fc570f06f7928d94e9d4911
NaN <nan@NaNs-Mac-mini.local>
2025-12-07 21:06:19 +0100

    add capacitor packages

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.letteron.app',
+  appName: 'LetterON AI Studio',
+  webDir: 'dist'
+};
+
+export default config;
